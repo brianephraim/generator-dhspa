7d42b8d6373fb808181db42005390c7532cf84f3
Brian Ephraim <brian@okcupid.com>
2014-03-25 14:16:14 -0400

    kill live grunt

--- feature/index.js
+++ feature/index.js
@@ -8,16 +8,15 @@ var FeatureGenerator = yeoman.generators.Base.extend({
   // init: function () {
     // console.log('You called the feature subgenerator with the argument ' + this.name + '.');
   // },
+  killLiveGrunt:function(){
+  	var path = this.dest._base+'/app/generated/utility/gruntreset.txt';
+  	this.dest['delete'](path)
+  	this.write(path,new Date())
+  },
   askFor: function () {
   	var done = this.async();
 
   	var prompts = [
-      // {
-      //   type: 'confirm',
-      //   name: 'someOption',
-      //   message: 'Would you like to enable this option?',
-      //   default: true
-      // },
       {
         type: 'input',
         name: 'featureName',
@@ -39,50 +38,10 @@ var FeatureGenerator = yeoman.generators.Base.extend({
       done();
     }.bind(this));
   },
-  routes: function(){
-  	var jsonPath = this.dest._base+'/app/config/routes.json';
-  	var parsedJSON = require(jsonPath);
-  	var self = this;
-  	parsedJSON[0].subRoutes.push({
-		"routeName":this.featureName,
-		"bundle":this.featureName,
-		"viewElm":"basexView"
-	})
-  	
-  	// this['remove'](jsonPath)
-  	this.dest['delete'](jsonPath)
-  	this.write(jsonPath,JSON.stringify(parsedJSON, null, 4))
-  	
-  	console.log('////////')
-  	console.log('////////')
-  	console.log('////////')
-  	console.log('////////')
-  	console.log('////////')
-  	console.log('////////')
-  	console.log(1)
-  	console.log('////////')
-  	console.log('////////')
-  	console.log('////////')
-  	console.log('////////')
-  	console.log('////////')
-  	console.log('////////')
-  },
+
   files: function () {
     var fs = require('fs');
-	console.log(fs.readdirSync('./'))
-	console.log('zxcvzxcv')
-	console.log(this.sourceRoot())
-	  // this.template('2013-11-12-<%= widgetNameAllLower %>.md','2013-11-12-<%= widgetNameAllLower %>.md');
-	  // this.template('css/app.css','css/app.css');
-	  // this.template('css/reset.css','css/reset.css');
-	  // this.template('css/<%= widgetNameAllLower %>.css','css/<%= widgetNameAllLower %>.css');
-	  // this.template('index-async.html','index-async.html');
-	  // this.template('index.html','index.html');
-	  // this.template('js/app.js','js/app.js');
-	  // this.template('js/<%= widgetNameAllLower %>.js','js/<%= widgetNameAllLower %>.js');
 
-	  //this.copy('_package.json', 'package.json');
-	  //this.copy('_bower.json', 'bower.json');
 	  var stuffDir = this.sourceRoot()+'/feature';
 
 	  
@@ -110,28 +69,37 @@ var FeatureGenerator = yeoman.generators.Base.extend({
 	    return a;
 	  }
 	  var filesArray = getFolderContentArray(stuffDir);
-	  console.log(filesArray)
-	  // var moment = require('moment')
 
 	  for(var i=0,l=filesArray.length;i<l;i++){
 	    var source = 'feature/'+filesArray[i];
-	    console.log(source)
 	    
 	    var dest = (filesArray[i]).replace('feature',this.featureName);
 	    dest = 'app/features/' + this.featureName + '/' + dest;
-	  //   // .replace('widgetYYYY-MM-DD',moment(new Date()).format("YYYY-MM-DD"));
-	  //   dest = dest === '_bower.json' ? 'bower.json' : dest;
-	  //   dest = dest === '_package.json' ? 'package.json' : dest;
-	  //   dest = dest === '_gitignore' ? '.gitignore' : dest;
-	  //   dest = dest === '_bowerrc' ? '.bowerrc' : dest;
-	  //   if(dest.indexOf('-notemplate') !== -1){
-	  //     dest = dest.replace('-notemplate','');
-	  //     console.log('------ '+dest)
-	  //     this.copy(source,dest);
-	  //   } else {
-	      this.template(source,dest);
-	  //   }
+	 
+	    this.template(source,dest);
 	  }
+  },
+  routes: function(){
+  	var jsonPath = this.dest._base+'/app/config/routes.json';
+  	var parsedJSON = require(jsonPath);
+  	var self = this;
+  	parsedJSON[0].subRoutes.push({
+		"routeName":this.featureName,
+		"bundle":this.featureName,
+		"viewElm":"basexView"
+	})
+  	
+  	this.dest['delete'](jsonPath)
+  	this.write(jsonPath,JSON.stringify(parsedJSON, null, 4))
+
+  	this.on('end', function () {
+	  this.installDependencies({
+	    skipInstall: this.options['skip-install'],
+	    callback: function () {
+	      this.spawnCommand('grunt', ['myTemplates:all','routesAndRequireConfigTasksDev']);
+	    }.bind(this) // bind the callback to the parent scope
+	  });
+	});
   }
 });
 
